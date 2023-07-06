934508a693e80cfc6f139837df907ab342898c7b
umidjon003 <umidjon.murodov.003@gmail.com>
2023-07-06 18:05:35 +0500

    1 commit

---
+++ home_work.js
@@ -0,0 +1,36 @@
+// let arr=[1,2,3]
+// let arr1=[1,2,3,4]
+// function x (arr,arr1){
+//   if (arr.length!== arr1.length){
+//     console.log ( arr.concat(arr1))
+//   }return
+//   for (i=0; i<=arr1.length;i++){
+//     if (arr[i]!==arr1[i]){
+//       console.log(arr)
+//       return
+//     }else {
+//       console.log(arr.concat(arr1))
+//     }
+//   }
+// }x(arr,arr1)
+
+// function getNumbersFromString(str) {
+//     let numbers = [];
+//     let number = "";
+//     for (let i = 0; i < str.length; i++) {
+//         if (str[i] >= '0' && str[i] <= '9') {
+//             number += str[i];
+//         } else {
+//             if (number.length) {
+//                 numbers.push(number);
+//                 number = "";
+//             }
+//         }
+//     }
+//     if (number.length) {
+//         numbers.push(number);
+//     }
+//     return numbers;
+// }
+
+
